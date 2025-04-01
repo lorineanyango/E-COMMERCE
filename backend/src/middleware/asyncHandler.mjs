40d2cbbf9c8f7d0e05c2ca59aa6777efb787abac
unknown <lori.anyango23@gmail.com>
2025-04-01 11:47:37 +0300

    final commit

--- backend/src/middleware/asyncHandler.mjs
+++
@@ -1,6 +0,0 @@
-export const asyncHandler = (fn)=>(req, res, next)=>{
-  Promise.resolve(fn(req, res, next)).catch(error=>{
-    res.status(500).send({message: error.message})
-    next();
-  });
-}
--- frontend/src/redux/features/constant.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL ='http://localhost:5000'
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/categorys";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
